b0d2c8f82d8dd9b7993ad839a3883172365ea196
Amit Kumar <amitkumar.sharma@hotmail.com>
2018-06-29 00:09:00 +0530

    First Commit

---
+++ GoogleLaunch/src/main/java/cioreview/Google.java
@@ -0,0 +1,18 @@
+package cioreview;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.Test;
+
+public class Google {
+
+	@Test
+	public void check()
+	{
+		// TODO Auto-generated method stub
+		System.setProperty("webdriver.gecko.driver","D:\\seleniumfile\\geckodriver-v0.21.0-win32\\geckodriver.exe");
+		WebDriver driver = new FirefoxDriver();
+		driver.get("https://www.google.com");
+	}
+
+}
